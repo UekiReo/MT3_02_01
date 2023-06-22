4f9ad78b2237a5f74053811b709f85feb24a32d8
k022g0005@g.neec.ac.jp <k022g0005@g.neec.ac.jp>
2023-06-22 14:10:43 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,105 @@
+#include <Novice.h>
+#include <MyMath.h>
+#include "ImGuiManager.h"
+
+const char kWindowTitle[] = "LE2B_04_ウエキレオ_MT3_02_01";
+
+const int kWindowWidth = 1280;
+const int kWindowHeight = 720;
+
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, kWindowWidth, kWindowHeight);
+
+	Vector3 cameraTranslate{ 0.0f, 1.9f, -6.49f };
+	Vector3 cameraRotate{ 0.26f, 0.0f, 0.0f };
+
+	Sphere sphere1{
+		{0.0f, 0.0f, 0.0f},
+		1.0f
+	};
+	Sphere sphere2{
+		{0.0f, 0.0f, 0.0f},
+		0.5f
+	};
+	uint32_t colorS1 = WHITE;
+	uint32_t colorS2 = WHITE;
+
+	// キー入力結果を受け取る箱
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		Matrix4x4 worldMatrix = MyMath::MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, { 0.0f, 0.0f, 0.0f });
+		Matrix4x4 cameraMatrix = MyMath::MakeAffineMatrix({ 1.0f, 1.0f, 1.0f }, cameraRotate, cameraTranslate);
+		Matrix4x4 viewMatrix = MyMath::Inverse(cameraMatrix);
+		Matrix4x4 projectionMatrix = MyMath::MakePerspectiveFovMatrix(0.45f, float(kWindowWidth) / float(kWindowHeight), 0.1f, 100.0f);
+		Matrix4x4 worldViewProjectionMatrix = MyMath::Multiply(worldMatrix, MyMath::Multiply(viewMatrix, projectionMatrix));
+		Matrix4x4 viewportMatrix = MyMath::MakeViewPortMatrix(0, 0, float(kWindowWidth), float(kWindowHeight), 0.0f, 1.0f);
+
+		if (MyMath::IsCollision(sphere1, sphere2)) {
+			colorS1 = RED;
+		}
+		else {
+			colorS1 = WHITE;
+		}
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+
+		MyMath::DrawShere(sphere1, worldViewProjectionMatrix, viewportMatrix, colorS1);
+		MyMath::DrawShere(sphere2, worldViewProjectionMatrix, viewportMatrix, colorS2);
+
+
+		///
+		/// ↓描画処理ここから
+		///
+
+
+
+		MyMath::DrawGrid(worldViewProjectionMatrix, viewportMatrix);
+
+
+
+		ImGui::Begin("Debug");
+		ImGui::DragFloat3("sphere1", &sphere1.center.x, 0.1f, -1.0f, 1.0f);
+		ImGui::DragFloat("sphere1", &sphere1.radius, 0.1f, 0.0f, 1.0f);
+		ImGui::DragFloat3("sphere2", &sphere2.center.x, 0.1f, -1.0f, 1.0f);
+		ImGui::DragFloat("sphere2", &sphere2.radius, 0.1f, 0.0f, 1.0f);
+
+		ImGui::End();
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
